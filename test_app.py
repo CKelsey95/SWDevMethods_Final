644955ab0a83a7d0a4aa591ed129e5dc4b8131dc
mnoor2 <mnoor2105@yahoo.com>
2024-04-17 19:19:47 -0600

    SF-15 fixed unit testing cases

--- test_app.py
+++ test_app.py
@@ -1,19 +1,42 @@
-from app import app as flask_app
 import pytest
-import json
+from app import app
 
 @pytest.fixture
-def app():
-    yield flask_app
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        with app.app_context():
+            yield client
 
-@pytest.fixture
-def client(app):
-    return app.test_client()
+def test_home_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
 
-def test_home(app, client):
-    res = client.get('/')
-    assert res.status_code == 200
-    
-def test_room(app, client):
-   res = client.get('/room')
-   assert res.status_code == 200 
+def test_room_route(client):
+    with client.session_transaction() as sess:
+        sess['room'] = 'test_room'
+        sess['name'] = 'test_user'
+    response = client.get('/room')
+    assert response.status_code == 302
+
+def test_message_event(client):
+    response = client.post('/', data={'name': 'test_user', 'code': 'test_room'})
+    assert response.status_code == 200
+    response = client.get('/room', follow_redirects=True)
+    assert response.status_code == 200
+
+def test_connect_event(client):
+    with client.session_transaction() as sess:
+        sess['room'] = 'test_room'
+        sess['name'] = 'test_user'
+    client.get('/room')
+    response = client.get('/', follow_redirects=True)
+    assert response.status_code == 200
+
+def test_disconnect_event(client):
+    with client.session_transaction() as sess:
+        sess['room'] = 'test_room'
+        sess['name'] = 'test_user'
+    client.get('/room')
+    response = client.get('/', follow_redirects=True)
+    assert response.status_code == 200
